8fc100fd52142922763ba6bccb1671716bda6ab6
Kevin S <kshen3778@gmail.com>
2017-11-19 18:03:47 +0000

    started views

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Path = require('path');
+const Hoek = require('hoek');
 const unirest = require('unirest');
 
 const base_url = "https://connect.squareup.com/v2";
+const config = require('./config.json');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -12,14 +15,49 @@ server.connection({
     port: 8080 
 });
 
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello', 
-    handler: function (request, reply) {
+function getItem(callback) {
+	unirest.get(base_url + '/catalog/object/NRVDGFKWJGOE6H4UY7MKAHGV')
+	.headers({
+		'Authorization': 'Bearer ' + config.squareAccessToken,
+		'Accept': 'application/json'
+	})
+	.end(function(response) {
+        console.log("first response");
+		callback(response, null);
+		return;
+		
+	});
+}
 
-        return reply('hello world');
-    }
+server.register(require('vision'), (err) => {
+    Hoek.assert(!err, err);
+
+    server.views({
+        engines: {
+            html: require('handlebars')
+        },
+        relativeTo: __dirname,
+        path: 'templates'
+    });
+    
+    // Add the route
+    server.route({
+        method: 'GET',
+        path:'/hello', 
+        handler: function (request, reply) {
+            
+            getItem(function(response, error){
+        		if (error) {
+        			reply(error);
+        		} else {
+        		    console.log(response.body);
+        			reply(response.body);
+        		}
+    	    });
+    
+        }
+        
+    });
 });
 
 // Start the server
